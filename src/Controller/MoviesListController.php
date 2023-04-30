57f2b143485c414067b512c7b989e12118ee238c
bore-prog <philippeaurore@orange.fr>
2023-04-30 02:09:13 +0200

    Update MovieListController with professor advises.

--- src/Controller/MoviesListController.php
+++ src/Controller/MoviesListController.php
@@ -3,18 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Mood;
-use App\Repository\MovieRepository;
-use App\Repository\MoodRepository;
+use App\Repository\{MovieRepository,MoodRepository};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MoviesListController extends AbstractController
+final class MoviesListController extends AbstractController
 {
     #[Route('/movies/list', name: 'app_movies_list')]
     public function index(MovieRepository $movieRepository): Response
     {
-
         return $this->render('movies_list/index.html.twig', [
             'movies' => $movieRepository->findAll(),
         ]);
@@ -23,7 +21,6 @@ class MoviesListController extends AbstractController
     #[Route('/movies/list/{mood}', name: 'app_movies_list_mood')]
     public function new(Mood $mood, MovieRepository $movieRepository): Response
     {
-
         return $this->render('movies_list/index.html.twig', [
             'movies' => $mood->getMovies(),
         ]);
